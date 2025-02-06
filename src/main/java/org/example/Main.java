e4e75c324526bd365f5b1033d9e99d1ca0894bc4
fabrice kouonang <97543674+LeFab70@users.noreply.github.com>
2025-02-06 00:57:33 -0400

    indexOf(), lastIndexOf(),subString(),chartAt()

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nameComplet = "Fabrice Michel".trim();
+        System.out.println(nameComplet.substring(1, 3));
+        String prenom = nameComplet.substring(0, nameComplet.indexOf(" "));
+        String name = nameComplet.substring(nameComplet.indexOf(" ") + 1);
+        System.out.println("Nom:" + name + " \nPrénom:" + prenom);
+        System.out.println("Nom :" + name + " " + prenom.charAt(0)+".");
+
+    }
+}
